a06e4f951e156a2fb674254b7e68c9e32e4e8677
Maksim <malyshev.maksim99@gmail.com>
2020-03-20 19:47:29 +0300

    Initial upload

---
+++ MusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Максим on 20.03.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
